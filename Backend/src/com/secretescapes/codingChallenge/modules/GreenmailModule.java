787d04d4d9c2a46e2763bf8138d25c8283f7936d
Vethan <vethan@unrulyattractions.com>
2017-02-21 12:51:37 +0000

    Add the user + move string constants to constants

--- Backend/src/com/secretescapes/codingChallenge/modules/GreenmailModule.java
+++ Backend/src/com/secretescapes/codingChallenge/modules/GreenmailModule.java
@@ -10,17 +10,23 @@ import javax.mail.Session;
 import java.util.Properties;
 
 public class GreenmailModule extends AbstractModule {
+    private static final String USER_PASSWORD = "abcdef123";
+    private static final String USER_NAME = "hascode";
+    private static final String EMAIL_USER_ADDRESS = "hascode@localhost";
+    private static final String LOCALHOST = "127.0.0.1";
+
     @Override
     protected void configure() {
-        bind(String.class).annotatedWith(Names.named("email_host")).toInstance("localhost");
-        bind(String.class).annotatedWith(Names.named("email_user_address")).toInstance("localhost");
-        bind(String.class).annotatedWith(Names.named("email_user_password")).toInstance("localhost");
+        bind(String.class).annotatedWith(Names.named("email_host")).toInstance(LOCALHOST);
+        bind(String.class).annotatedWith(Names.named("email_user_address")).toInstance(EMAIL_USER_ADDRESS);
+        bind(String.class).annotatedWith(Names.named("email_user_password")).toInstance(USER_PASSWORD);
 
         GreenMail greenMail = new GreenMail(ServerSetupTest.SMTP);
         greenMail.start();
+        greenMail.setUser(EMAIL_USER_ADDRESS, USER_NAME, USER_PASSWORD);
 
         Properties props = System.getProperties();
-        props.put("mail.smtp.host", "localhost");
+        props.put("mail.smtp.host", LOCALHOST);
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", ServerSetupTest.SMTP.getPort());
         Session session = Session.getInstance(props, null);
